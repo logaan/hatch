db2ac5af280ff60151a14c940dad8f2099f2ea79
Andy Kitchen <kitchen.andy+git@gmail.com>
2014-07-16 22:58:29 +1000

    improved entity rendering

--- src/wc/ui/core.cljs
+++ src/wc/ui/core.cljs
@@ -8,33 +8,69 @@
 
 (def app-state (atom {}))
 
+(defn on-entity! [ent]
+  (swap! app-state assoc :entity ent)
+  #_(debug/inspect! @app-state))
+
+(defn present-entity! [href]
+  (xhr/req
+   {:method :get
+    :url href
+    :on-complete on-entity!}))
+
+(defn entity-link [{:keys [href rel]}]
+  (letfn [(on-click [ev]
+            (.preventDefault ev)
+            (present-entity! href))]
+    (dom/a #js{:href href :onClick on-click} (first rel))))
+
+(defn wrap-list [title ls]
+  (when (not (empty? ls))
+    (dom/div nil
+            (dom/h5 nil title)
+            (apply dom/ul nil
+                   (map #(dom/li nil %) ls)))))
+
+(defn entities-list [ent]
+  (wrap-list "Entities"
+             (om/build-all entity (:entities ent))))
+
+(defn links-list [ent]
+  (wrap-list "Links"
+             (map entity-link (:links ent))))
+
+(defn actions-list [ent]
+  (wrap-list "Actions"
+             (for [action (:actions ent)]
+               (:title action))))
+
+(defn display-name [ent]
+  (->> ent
+       :properties
+       (filter (fn [[k v]] (= "name" (name k))))
+       first second))
+
 (defn entity [ent owner]
   (reify
     om/IRender
     (render [this]
       (dom/div
        nil
-       (dom/h4 nil (-> ent :properties :host/name)
-       (apply dom/ul nil
-              (om/build-all entity (:entities ent))))))))
+       (dom/h4 nil (display-name ent))
+       (links-list ent)
+       (entities-list ent)
+       (actions-list ent)
+       ))))
 
 (defn app [data owner]
   (reify
     om/IRender
     (render [this]
-      (om/build entity (:data data)))))
+      (om/build entity (:entity data)))))
 
 (defn render! []
   (om/root app app-state
    {:target (. js/document (getElementById "app"))}))
 
-(defn load-data [data]
-  (swap! app-state assoc :data data)
-  (debug/inspect! @app-state))
-
-(xhr/req
- {:method :get
-  :url "/hosts"
-  :on-complete load-data})
-
+(present-entity! "/hosts")
 (render!)
