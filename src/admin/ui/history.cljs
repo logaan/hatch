cef075921f3f41ec0e59cde071485555851472d0
Andy Kitchen <kitchen.andy+git@gmail.com>
2014-08-01 23:30:27 +1000

    use onhashchange instead of janky google closure history

--- src/admin/ui/history.cljs
+++ src/admin/ui/history.cljs
@@ -1,26 +1,17 @@
 (ns admin.ui.history
-  (:require [goog.events :as events]
-            [uri.core    :as uri])
-  (:import goog.history.Html5History
-           goog.history.EventType))
+  (:require [uri.core :as uri]))
 
-(def history (new goog.history.Html5History))
-
-(defn defer [f]
-  (js/setTimeout f 0))
+(defn location-hash []
+  (.slice js/window.location.hash 1))
 
 (defn watch [f]
-  (defer
-    (fn []
-      (events/listen history goog.history.EventType.NAVIGATE
-                     (fn [e] (f (.-token e))))
-      (.setEnabled history true))))
+  (letfn [(onhashchange [_] (f (location-hash)))]
+    (.addEventListener
+     js/window
+     "hashchange"
+     onhashchange
+     false)
+    (js/setTimeout onhashchange 0)))
 
 (defn goto! [loc]
-  (.setToken history (uri/relative loc)))
-
-(defn replace! [loc]
-  (.replaceToken history (uri/relative loc)))
-
-(defn location []
-  (.getToken history))
+  (aset js/window.location "hash" (uri/relative loc)))
