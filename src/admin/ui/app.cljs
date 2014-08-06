1a5111ba244b7332e41a0472f9a4a3855d1785a6
Andy Kitchen <kitchen.andy+git@gmail.com>
2014-08-06 14:12:51 +1000

    make use of !-suffix more consistent

--- src/admin/ui/app.cljs
+++ src/admin/ui/app.cljs
@@ -12,7 +12,7 @@
 
 (declare present!)
 (declare reload!)
-(declare on-entity-ok)
+(declare on-entity-ok!)
 
 (def http-ok         200)
 (def http-created    201)
@@ -20,7 +20,7 @@
 
 (defn on-response! [cursor res]
   (condp = (.getStatus res)
-    http-ok         (on-entity-ok cursor (xhr/->edn res))
+    http-ok         (on-entity-ok! cursor (xhr/->edn res))
     http-created    (history/goto! (.getResponseHeader res "Location"))
     http-no-content (history/goto! (siren/self (:entity @cursor)))
     ))
@@ -45,34 +45,34 @@
      :on-complete
      (fn [res ev] (on-response! cursor res))})))
 
-(defn show-action-form [cursor act]
+(defn show-action-form! [cursor act]
   (om/update! cursor :form
-              {:action act
-               :values {}
-               :back-href (str "#" (:entity-url @cursor))
-               :on-submit
-               (fn [action values]
-                 (exec-action! cursor @action @values))}))
+    {:action act
+     :values {}
+     :back-href (str "#" (:entity-url @cursor))
+     :on-submit
+     (fn [action values]
+       (exec-action! cursor @action @values))}))
 
-(defn set-pending-action [cursor act-name]
+(defn set-pending-action! [cursor act-name]
   (om/update! cursor :pending-action act-name))
 
-(defn do-action [cursor act-name]
+(defn do-action! [cursor act-name]
   (when-let [act (siren/get-action (:entity @cursor) act-name)]
-    (show-action-form cursor act)))
+    (show-action-form! cursor act)))
 
-(defn do-pending-action [cursor]
-  (do-action cursor (:pending-action @cursor))
+(defn do-pending-action! [cursor]
+  (do-action! cursor (:pending-action @cursor))
   (om/update! cursor :pending-action nil))
 
-(defn clear-current-action [cursor]
+(defn clear-current-action! [cursor]
   (om/update! cursor :form nil))
 
 (defn present-action-form! [cursor act]
   (let [url (str (:entity-url @cursor) "#" (:name act))]
     (om/update! cursor :url url)
     (history/goto! url)
-   (show-action-form cursor act)))
+    (show-action-form! cursor act)))
 
 (defn update-all-in [m ks f arg]
   (update-in m ks (fn [m] (map #(f arg %) m))))
@@ -98,7 +98,7 @@
         ent (add-action-handlers cursor ent)]
     ent))
 
-(defn on-entity-ok [cursor ent]
+(defn on-entity-ok! [cursor ent]
   (let [self (uri/relative (siren/self ent))]
     (when (not= self (:entity-url @cursor))
       (om/update! cursor :entity-url self)
@@ -106,10 +106,10 @@
 
   (let [ent+ (add-handlers cursor ent)]
    (om/update! cursor :entity ent+)
-   (clear-current-action cursor)
-   (do-pending-action cursor)))
+   (clear-current-action! cursor)
+   (do-pending-action! cursor)))
 
-(defn get-entity [cursor href]
+(defn get-entity! [cursor href]
   (loading/begin-loading! cursor)
   (xhr/req
    {:method "GET"
@@ -121,25 +121,25 @@
         (loading/finish-loading! cursor)
         (on-response! cursor res)))}))
 
-(defn request-entity [cursor href]
+(defn request-entity! [cursor href]
   (when-let [req (:current-request @cursor)]
     (.abort req))
-  (let [req (get-entity cursor href)]
+  (let [req (get-entity! cursor href)]
     (om/update! cursor :current-request req)))
 
-(defn load-entity [cursor href]
+(defn load-entity! [cursor href]
   (om/update! cursor :entity-url href)
-  (request-entity cursor href))
+  (request-entity! cursor href))
 
 (defn reload! [cursor]
-  (request-entity cursor (:entity-url @cursor)))
+  (request-entity! cursor (:entity-url @cursor)))
 
 (defn present! [cursor href]
   (when (not= href (:url @cursor))
     (let [[base frag] (uri/split-fragment href)]
       (om/update! cursor :url href)
-      (set-pending-action cursor frag)
-      (load-entity cursor base))))
+      (set-pending-action! cursor frag)
+      (load-entity! cursor base))))
 
 (defn component [data owner]
   (reify
