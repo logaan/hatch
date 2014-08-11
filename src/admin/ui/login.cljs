b0e18d26ccfcf4f813d25849e2707f07f1b0a771
Andy Kitchen <kitchen.andy+git@gmail.com>
2014-08-11 18:46:30 +1000

    added login component

---
+++ src/admin/ui/login.cljs
@@ -0,0 +1,43 @@
+(ns admin.ui.login
+  (:require [om.core :as om  :include-macros true]
+            [om.dom  :as dom :include-macros true]))
+
+(defn update-key [cursor k]
+  (fn [e] (om/update! cursor k (.. e -target -value))))
+
+(defn component
+  [{:keys [username password on-submit] :as cursor} owner]
+  (om/component
+   (dom/form
+    #js{:className "form-signin"
+        :role      "form"
+        :onSubmit  (fn [e]
+                     (.preventDefault e)
+                     (when on-submit
+                       (on-submit
+                        (select-keys
+                         @cursor
+                         [:username :password]))))}
+    (dom/h2
+     #js{:className "form-signin-heading"}
+     "Please sign in")
+    (dom/input
+     #js{:type "text"
+         :className "form-control"
+         :placeholder "Username"
+         :required true
+         :autofocus true
+         :value username
+         :onChange (update-key cursor :username)})
+    (dom/input
+     #js{:type "password"
+         :className "form-control"
+         :placeholder "Password"
+         :required true
+         :value password
+         :onChange (update-key cursor :password)})
+    (dom/button
+     #js{:type "submit"
+         :className "btn btn-primary bnt-block"}
+     "Sign in")
+    )))
--- test/admin/ui/components_test.cljs
+++ test/admin/ui/components_test.cljs
@@ -2,9 +2,13 @@
   (:require [om.core :as om]
             [admin.ui.test-data :as test-data]
             [admin.ui.entity :as entity]
-            [admin.ui.action :as action]))
+            [admin.ui.action :as action]
+            [admin.ui.login  :as login]))
 
 (defn render! []
+  (om/root login/component
+           {:on-submit #(js/alert (pr-str %))}
+           {:target (js/document.getElementById "login")})
   (om/root entity/component test-data/hosts-data
            {:target (js/document.getElementById "entity-hosts")})
   (om/root entity/component test-data/ent-w-props
